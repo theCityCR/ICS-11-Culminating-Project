7c4d22b4b7b702cdfdd8a0730c3f8b89d1d2818e
Alex Zhu <alexyifanzhu@gmail.com>
2022-12-20 20:56:35 -0500

    Fixed buggy home button. Also half of the focus timer logic but hhmmss(time) isn't updating even though time is

--- FocusfyScript.js
+++ FocusfyScript.js
@@ -1,9 +1,9 @@
-/**
+/*
  * @author Raymond, Sean, Alex
  */
-
+timerStart=false;
 function openTab(evt, cityName) {
-    let i, tabcontent, tablinks;
+    let i, tabcontent, tablinks,tablinksHome;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -12,12 +12,62 @@ function openTab(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    tablinksHome = document.getElementsByClassName("tablinksHome");
+    for (i = 0; i < tablinksHome.length; i++) {
+        tablinksHome[i].className = tablinksHome[i].className.replace(" active", "");
+    }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+    //evt.currentTarget.className += " active";
+
+
 }
+let time = 1800000
+document.getElementById("timeleft").innerHTML=hhmmss(time);
 
-document.getElementById("defaultOpen").click();
+function hhmmss(miliseconds)
+{
+    let hours=Math.floor(miliseconds/3600000);
+    let minutes=Math.floor((miliseconds-hours*3600000)/60000);
+    let seconds=Math.floor((miliseconds-hours*3600000-minutes*60000)/1000);
 
-const d = new Date().toLocaleString();
-document.getElementById("date").value = d;
-console.log(d);
+    return (""+hours+":"+minutes+":"+seconds);
+}
+function startTimer()
+{
+    timerStart=!timerStart;
+    if (timerStart) {
+        setInterval(function(){
+            time = time - 1;
+            document.getElementById("timeleft").innerHTML=hhmmss(time);
+            console.log(hhmmss(time));
+        }, 1000);
+    }
+}
+/*
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);*/
