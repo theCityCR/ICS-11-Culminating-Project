cae6dbddae3b1a20d8a57a9656176bb4914d35d9
Raymond <raymond.ouyang@student.tdsb.on.ca>
2022-12-21 10:41:11 -0500

    Changed table.

--- FocusfyScript.js
+++ FocusfyScript.js
@@ -16,8 +16,15 @@ function openTab(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+function addSchedule(event) {
+    const schedule = event.target.files[0];
+    
+}
+
 document.getElementById("defaultOpen").click();
 
 const d = new Date().toLocaleString();
 document.getElementById("date").value = d;
 console.log(d);
+
+document.getElementById("scheduleUpload").addEventListener("change", addSchedule);
