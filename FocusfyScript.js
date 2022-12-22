44e8986b0ddd758979879f6d3a75c91545e888f2
Buddhateh18th <alexyifanzhu@gmail.com>
2022-12-22 09:49:50 -0500

    Fixed buggy home button

--- FocusfyScript.js
+++ FocusfyScript.js
@@ -3,7 +3,7 @@
  */
 
 function openTab(evt, cityName) {
-    let i, tabcontent, tablinks;
+    let i, tabcontent, tablinks, tablinksHome;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -11,6 +11,10 @@ function openTab(evt, cityName) {
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    tablinksHome = document.getElementsByClassName("tablinksHome");
+    for (i = 0; i < tablinksHome.length; i++) {
+        tablinksHome[i].className = tablinksHome[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
