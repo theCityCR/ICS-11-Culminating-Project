a25dbe5fb49a44103d2810d16ac0dd75152d6164
Alex Zhu <alexyifanzhu@gmail.com>
2022-12-28 11:35:48 -0500

    Work timer and break timer finished, most of the theme is implemented other than the blue break timer.

--- TimerScript.js
+++ TimerScript.js
@@ -1,6 +1,6 @@
 timerStart=false; //To check if the timer should be running
 workTime=true;//show work timer not break timer
-timerRunning=false;
+timerRunning=true;
 
 
 
@@ -42,144 +42,40 @@ function startButtonClick() {
     timerStart = !timerStart;
     if (timerStart) {
 
-        if (workTime){
+        if (workTime&&timerRunning){
             time = 1800000; //default time of 30:00
             document.getElementById("timeleft").innerHTML = hhmmss(time); //Set the timer to display 30:00 not 1800000
-            let timerRepeatId = setInterval(function () {
-
-                //startButtonDOM.className+=" active";
-                time = time - 1000;
-                document.getElementById("timeleft").innerHTML = hhmmss(time);
-                document.getElementById("startButton").innerHTML = "Stop timer"; //change the button
-
-                if (!timerStart) //If we should stop the timer
-                {
-                    document.getElementById("startButton").innerHTML = "Start timer";
-                    //startButtonDOM.className.replace(" active", "");
-                    clearInterval(timerRepeatId); //stop the interval
-                }
-            }, 1000); //every 1 second the timer value is reevaluated
+            document.getElementById("timerHeader").innerHTML="Time to work!";
         }
 
-        else {
+        else if (!workTime&&timerRunning){
             time = 300000; //default time of 30:00
-            document.getElementById("timeleft").innerHTML = hhmmss(time); //Set the timer to display 30:00 not 1800000
-            let timerRepeatId = setInterval(function () {
-                //startButtonDOM.className+=" active";
-                time = time - 1000;
-                document.getElementById("timeleft").innerHTML = hhmmss(time);
-                document.getElementById("startButton").innerHTML = "Stop timer"; //change the button
-
-                if (!timerStart) //If we should stop the timer
-                {
-                    document.getElementById("startButton").innerHTML = "Start timer";
-                    //startButtonDOM.className.replace(" active", "");
-                    clearInterval(timerRepeatId); //stop the interval
-                }
-            }, 1000); //every 1 second the timer value is reevaluated
+            document.getElementById("timeleft").innerHTML = hhmmss(time); //Set the timer to display 5:00 not 300000
+            document.getElementById("timerHeader").innerHTML="Take a break!";
         }
-    }
-}/*
-function startTimer() //function for when the start timer button is clicked
-{
-    if (!timerRunning) {
-        console.log("hi");
-        timerRunning=true;
-        console.log(timerRunning);
-        if (workTime) {
-            time = 1800000; //default time of 30:00
-            document.getElementById("timeleft").innerHTML = hhmmss(time); //Set the timer to display 30:00 not 1800000
-            timerStart = false;
-            console.log(timerRunning);
-            timerStart=!timerStart; //Change what the function does on the next click
-            console.log(timerRunning);
-            if (timerStart) {
-                console.log(timerRunning);
-                let timerRepeatId=setInterval(function(){
 
-                    console.log(timerRunning);
-                    //startButtonDOM.className+=" active";
-                    time = time - 1000;
-                    document.getElementById("timeleft").innerHTML=hhmmss(time);
-                    document.getElementById("startButton").innerHTML="Stop timer"; //change the button
+        let timerRepeatId = setInterval(function () {
 
-                    if (!timerStart) //If we should stop the timer
-                    {
-                        document.getElementById("startButton").innerHTML="Start timer";
-                        //startButtonDOM.className.replace(" active", "");
-                        clearInterval(timerRepeatId); //stop the interval
-                    }
+            //startButtonDOM.className+=" active";
 
-                    if (time<=0) {
-                        workTime = !workTime;
-                        //startButtonDOM.className.replace(" active", "");
-                        clearInterval(timerRepeatId);
-                    }
-                }, 1000); //every 1 second the timer value is reevaluated
-            }
             timerRunning=false;
-        } else {
-            time = 300000 //default time of 5:00
-            document.getElementById("timeleft").innerHTML = hhmmss(time); //Set the timer to display 30:00 not 1800000
-            timerStart = false;
-            //breakTimer();
-        }
-    }
-
-
-}*/
-function test(timerRunning) {
-    console.log(timerRunning);
-    //startButtonDOM.className+=" active";
-    time = time - 1000;
-    document.getElementById("timeleft").innerHTML=hhmmss(time);
-    document.getElementById("startButton").innerHTML="Stop timer"; //change the button
-
-
-}
-function workTimer(){
-    timerStart=!timerStart; //Change what the function does on the next click
-    console.log(timerRunning);
-    if (timerStart) {
-        console.log(timerRunning);
-        let timerRepeatId=setInterval(function(){
-
-            test(timerRunning);
-
-            if (!timerStart) //If we should stop the timer
-            {
-                document.getElementById("startButton").innerHTML="Start timer";
-                //startButtonDOM.className.replace(" active", "");
-                clearInterval(timerRepeatId); //stop the interval
-            }
-
-            if (time<=0) {
-                workTime = !workTime;
-                //startButtonDOM.className.replace(" active", "");
-                clearInterval(timerRepeatId);
-            }
-        }, 1000,timerRunning); //every 1 second the timer value is reevaluated
-    }
-    timerRunning=false;
-}
-/*
-function breakTimer() {
-    timerStart=!timerStart; //Change what the function does on the next click
-    if (timerStart) {
-        let timerRepeatId=setInterval(function(){
+            document.getElementById("startButton").innerHTML = "Stop timer"; //change the button
             time = time - 1000;
-            document.getElementById("timeleft").innerHTML=hhmmss(time);
-            document.getElementById("startButton").innerHTML="Stop timer"; //change the button
+            document.getElementById("timeleft").innerHTML = hhmmss(time);
+
             if (!timerStart) //If we should stop the timer
             {
-                document.getElementById("startButton").innerHTML="Start timer";
+                document.getElementById("startButton").innerHTML = "Start timer";
+                //startButtonDOM.className.replace(" active", "");
                 clearInterval(timerRepeatId); //stop the interval
             }
 
-            if (time<=0){
+            if (time<1000){
                 workTime=!workTime;
+                timerStart=false;
+                timerRunning=true;
                 clearInterval(timerRepeatId);
             }
         }, 1000); //every 1 second the timer value is reevaluated
     }
-}*/
+}
