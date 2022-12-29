ab5e86e3ad58b144f32d68bc0b6131693bd2b82f
Alex Zhu <alexyifanzhu@gmail.com>
2022-12-29 11:08:53 -0500

    Formatting for the to do list is mostly done, just need to finish the hovering and active classes. Also fixed adding empty tasks.

--- ToDoListScript.js
+++ ToDoListScript.js
@@ -1,13 +1,16 @@
 tasks=document.getElementsByClassName("task");
 
 function addTask(){
+
     newTask = document.createElement("li"); //New task element in the list
     taskValue = document.createTextNode(document.getElementById("taskAdder").value);//take the taskvalue from the input
     newTask.appendChild(taskValue); //add the task value as the value for the new task
+    if (document.getElementById("taskAdder").value !== '') {
+        document.getElementById("taskAdder").value = "";
+        const checklist = document.getElementById("checklist");
+        checklist.append(newTask);
+    }
 
-    document.getElementById("taskAdder").value = "";//clear the input
 
-    const currentDiv = document.getElementById("checklist");
-    currentDiv.append(newTask);
 
 }
