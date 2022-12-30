12a107a3f27220d257d94b0ee307bd397c0978b0
Alex Zhu <alexyifanzhu@gmail.com>
2022-12-30 10:55:56 -0500

    To do list finished

--- ToDoListScript.js
+++ ToDoListScript.js
@@ -1,16 +1,51 @@
 tasks=document.getElementsByClassName("task");
+x=document.getElementsByClassName("closeTask");
 
+for (i=0;i<tasks.length;i++){
+    tasks[i].addEventListener("click",checkTask);
+    x[i].addEventListener("click",closeTask);
+}
 function addTask(){
 
-    newTask = document.createElement("li"); //New task element in the list
+    newRow = document.createElement("tr"); //New task element in the list
+    newTask=document.createElement("td")
+    newTask.className="task";
+    newX=document.createElement("td");
+    newX.className="closeTask";
     taskValue = document.createTextNode(document.getElementById("taskAdder").value);//take the taskvalue from the input
     newTask.appendChild(taskValue); //add the task value as the value for the new task
+    newX.appendChild(document.createTextNode("✖"));
+
     if (document.getElementById("taskAdder").value !== '') {
         document.getElementById("taskAdder").value = "";
-        const checklist = document.getElementById("checklist");
-        checklist.append(newTask);
+
+        newRow.append(newTask);
+        newRow.append(newX);
+
+        checklist = document.getElementById("checklist");
+        checklist.append(newRow);
     }
 
+    newTask.addEventListener("click",checkTask);
+    newX.addEventListener("click",closeTask);
+}
 
+function checkTask(event){
+    task=event.currentTarget
+    check=task.previousElementSibling;
+    if (task.style.textDecoration!== "line-through"){
+        task.style.textDecoration = "line-through";
+    }
+    else {
+        task.style.textDecoration = "";
+    }
 
 }
+
+function closeTask(event){
+    taskRow=event.currentTarget.parentElement;
+    taskRow.remove();
+}
+
+
+
