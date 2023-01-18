fee28588135388d124a3e32938b7e1967be7717e
Cqctxs <seanzhao2007@gmail.com>
2023-01-18 11:06:44 -0500

    changes to schedule

--- ScheduleScript.js
+++ ScheduleScript.js
@@ -1,6 +1,26 @@
 numRows = 0;
+let taskArr = [];
+let timeArr = [];
+
+// p u bli c s t a t i c i n t b i n a r y S e a r c h I t e r a t i v e ( i n t v ) {
+//     i n t l = 0 ;
+//     i n t r = a r r . l e n g t h − 1 ;
+//     w hil e ( l <= r ) {
+//         i n t m = ( l + r ) / 2 ;
+//         i f ( a r r [m] == v )
+//         r e t u r n m;
+//         i f ( a r r [m] > v )
+//         r = m − 1 ;
+//         e l s e
+//         l = m + 1 ;
+//     }
+//     r e t u r n −l − 1 ;
+// }
+
 function addSchedule(event) {
     let day = document.getElementById("daySelect").value;
+    let start = document.getElementById("timeInput").value;
+    let end = document.getElementById("timeInput2").value;
     let scheduleTable = document.getElementById(day);
     let newRow = document.createElement("tr");
     let newData = document.createElement("td");
@@ -8,17 +28,29 @@ function addSchedule(event) {
     newDiv.className = "scheduleDiv";
     newRow.className = "scheduleRow";
     newData.className = "scheduleData";
-    let scheduleValue = document.createTextNode(document.getElementById("addSchedule").value);
+    let taskName = document.getElementById("addSchedule").value;
+    let scheduleValue = document.createTextNode(taskName);
+    let newLine = document.createElement("br");
+    let scheduleTime = document.createTextNode( start + ' - ' + end);
     if (document.getElementById("addSchedule").value !== '') {
-        document.getElementById("addSchedule").value = "";
+        taskArr.push(taskName);
+        taskArr.push(start);
+        console.log(scheduleValue);
+        console.log(start);
 
-        newDiv.appendChild(scheduleValue);
-        newData.append(newDiv);
+        document.getElementById("addSchedule").value = "";
+        newData.appendChild(scheduleValue);
+        newData.appendChild(newLine);
+        newData.appendChild(scheduleTime);
         newRow.append(newData);
         scheduleTable.append(newRow);
         newData.addEventListener("click",deleteSchedule);
     }
+    let arr=document.getElementById("sunday").getElementsByTagName("td");
+    console.log(arr);
 }
+
+
 
 function deleteSchedule(event) {
     let task = event.currentTarget;
